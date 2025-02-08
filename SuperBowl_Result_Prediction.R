0a92ec8ded932f2d1cba162bb66d16ba56dfc727
joeylstarr <joeylstarr@yahoo.com>
2025-02-08 11:25:34 -0500

    Update SuperBowl_Result_Prediction.R

--- SuperBowl_Result_Prediction.R
+++ SuperBowl_Result_Prediction.R
@@ -15,11 +15,12 @@ colnames(nfl)
 eagles_stats <- nfl %>% filter(nfl$Team_games == "Eagles")
 chiefs_stats <- nfl %>% filter(nfl$Team_games == "Chiefs")
 
-nfl <- nfl%>% left_join(avg_opponent_ranking, by = c("Team_games" = "Team_games"))
+
 #Utilize average rankings based on strength of opponent
 avg_opponent_ranking <- nfl %>% group_by(Team_games) %>% summarize(avg_opponet_ranking = mean(opponent_rank, na.rm = TRUE)
 )
 avg_opponent_ranking
+nfl <- nfl%>% left_join(avg_opponent_ranking, by = c("Team_games" = "Team_games"))
 #Add the adjusted ranking rows into the dataset
 adjusted_ranks <- nfl %>% mutate(across(c(points_for, points_allowed, passing__yards, passing_tds, passing_ints, pass_yard_allowed, pass_td_allowed, rushing_yards, rushing_tds, rush_yard_allowed, rush_td_allowed), ~ . * avg_opponet_ranking, .names = "adjusted_{.col}"))
 adjusted_ranks
@@ -39,7 +40,7 @@ average_adjusted_rush_td_allowed = mean(adjusted_rush_td_allowed, na.rm = TRUE) 
 average_adjusted_passing_ints = mean(adjusted_passing_ints, na.rm = TRUE  ) / 10)
 
 #create the model that will compare each team vs the other
-model <- glm(adjusted_points_for ~ adjusted_passing__yards + adjusted_rushing_yards + adjusted_passing_tds + adjusted_rushing_tds - adjusted_passing_ints / (adjusted_pass_yard_allowed + adjusted_pass_td_allowed + adjusted_rush_yard_allowed + adjusted_rush_td_allowed), data = adjusted_ranks)
+model <- glm(adjusted_points_for ~ (adjusted_passing__yards + adjusted_rushing_yards + adjusted_passing_tds + adjusted_rushing_tds - adjusted_passing_ints) / (adjusted_pass_yard_allowed + adjusted_pass_td_allowed + adjusted_rush_yard_allowed + adjusted_rush_td_allowed), data = adjusted_ranks)
 model
 #Generate game data based on each stat category
 game <- data.frame(
